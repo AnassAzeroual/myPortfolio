869f8614edf0683e24b16c1c243a9d8fa312a4f6
AnassAzeroual <azeroual.anass2015@mail.com>
2021-06-17 17:48:52 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// Components
+import { ContactComponent } from './components/contact/contact.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceProfessionnelleComponent } from './components/experience-professionnelle/experience-professionnelle.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/me', pathMatch: 'full' },
+  { path: 'me', component: AboutmeComponent },
+  { path: 'ex', component: ExperienceProfessionnelleComponent },
+  { path: 'ed', component: EducationComponent },
+  { path: 'pr', component: ProjectsComponent },
+  { path: 'co', component: ContactComponent },
+  { path: '**', component: AboutmeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/experience-professionnelle/experience-professionnelle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-professionnelle',
+  templateUrl: './experience-professionnelle.component.html',
+  styleUrls: ['./experience-professionnelle.component.scss']
+})
+export class ExperienceProfessionnelleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
